5488eef0cb66221c02b8e53c83183b9d0215b4fe
bobtheetester <mdsajid3130@gmail.com>
2024-03-25 16:05:06 +0530

    second way of fibonaaci series

--- ex.py
+++ ex.py
@@ -1,12 +1,22 @@
-def fibonaccis(i):
-    if i<=0:
-     return i
-    else:
-        return(fibonaccis(i-1) + fibonaccis(i-2))
-num=int(input("Enter"))
-if num<=0:
-    print("enter a +ve number")
-else:
- print("fibonaccis:")
+#  def fibonaccis(i):
+#     if i<=0:
+#      return i
+#     else:
+#         return(fibonaccis(i-1) + fibonaccis(i-2))
+# num=int(input("Enter"))
+# if num<=0:
+#     print("enter a +ve number")
+# else:
+#  print("fibonaccis:")
+# for i in range(num):
+#      print(fibonaccis(i))
+
+
+num=10
+n1,n2=0,1
+print("fibonacci series of:",n1 ,n2)
 for i in range(num):
-    print(fibonaccis(i))
+    n3=n1+n2
+    n1=n2
+    n2=n3
+    print(n3,end="")
